9c5b5bf6206e0fab2771249398df06b98b8351f3
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-05-29 22:57:03 -0300

    AnimeSan: init application

---
+++ android/app/src/main/kotlin/com/ahnitam/animesan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahnitam.animesan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/animesan.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AnimeSan extends StatelessWidget {
+  const AnimeSan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.grey[500],
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      themeMode: ThemeMode.dark,
+    );
+  }
+}
