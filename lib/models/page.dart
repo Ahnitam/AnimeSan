ba65b6c60ab5352355ef62092f0e4316bd014f3b
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-06-20 15:53:31 -0300

    chore: delete files

--- lib/models/page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class PageModel {
-  final String titulo;
-  final Widget page;
-  final Icon icon;
-
-  PageModel({required this.titulo, required this.page, required this.icon});
-}
