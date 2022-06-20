839203846cbb4f89a5d3c2dc4776aadef68e39b2
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-06-20 15:58:21 -0300

    chore: add main files

--- lib/animesan.dart
+++ lib/animesan.dart
@@ -1,12 +1,14 @@
+import 'package:animesan/views/config.dart';
 import 'package:animesan/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AnimeSan extends StatelessWidget {
   const AnimeSan({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.green,
@@ -18,7 +20,11 @@ class AnimeSan extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black,
       ),
       themeMode: ThemeMode.dark,
-      home: Home(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Home(),
+        "/config": (context) => const Config(),
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:animesan/animesan.dart';
+import 'package:animesan/controllers/login_controller.dart';
+import 'package:animesan/controllers/module_controller.dart';
+import 'package:animesan/controllers/settings_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  await dependencies();
   runApp(const AnimeSan());
+}
+
+Future<void> dependencies() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences prefer = Get.put<SharedPreferences>(await SharedPreferences.getInstance());
+  final ModuleController moduleController = Get.put<ModuleController>(ModuleController(prefer: prefer));
+  Get.put(SettingsController(moduleController: moduleController, prefer: prefer));
+  Get.put(LoginController(moduleController: moduleController, prefer: prefer));
 }
