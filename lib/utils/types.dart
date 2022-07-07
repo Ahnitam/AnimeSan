62edd51df83b4a6f9489d8ddba8cc92457591f2e
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-07-07 14:27:07 -0300

    add: new types

--- lib/utils/types.dart
+++ lib/utils/types.dart
@@ -1,3 +1,13 @@
 enum MediaType { leg, dub, dual }
 
+enum DownloadType { softsub, hardsub }
+
+enum IdiomaType { none, ptBR, enUS, jaJP }
+
 enum QualidadeDownload { pior, melhor }
+
+enum ImageType { poster, wide, thumb }
+
+enum StreamType { video, audio, legenda }
+
+enum EpisodeType { leg, dub }
